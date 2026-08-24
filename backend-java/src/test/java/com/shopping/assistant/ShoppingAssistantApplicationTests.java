8c03bcb13a5c7f80521af1be1bea88e970d1a7cc
pranjalkumar09 <coderkumarshukla@gmail.com>
2026-08-24 13:59:16 +0530

    Implement ShoppingAssistantApplicationTests.java logic

---
+++ backend-java/src/test/java/com/shopping/assistant/ShoppingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
